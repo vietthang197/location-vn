305d569eb8a557e5afa5800c0b3893e3be3e7119
asus <levietthang1997@gmail.com>
2021-02-07 14:02:13 +0700

    Initial commit

---
+++ src/main/java/com/thanglv/locationvn/LocationVnApplication.java
@@ -0,0 +1,13 @@
+package com.thanglv.locationvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thanglv/locationvn/LocationVnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanglv.locationvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
